e1d2afcc14c9c258730dbd8bc25fd0f97ddb9961
Stephanie La Raja <stephanielaraja@gmail.com>
2021-10-09 13:05:01 -0400

    done

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import './App.css';
 class App extends Component {
 	handleOnClick = () => {
 		this.props.store.dispatch({
+			//dispatch method comes from the store (Redux library)
 		  type: 'INCREASE_COUNT',
 		});
 	  }
+
+//store contains two methods, dispatch and getState
 
 	render() {
 		return (
--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
 import counterReducer from './reducers/counterReducer.js';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//passes a reducer to the method
+const store = createStore(
+    counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+
+//passes createStore() method to our App comp as a prop
+ReactDOM.render(
+<App store={store} />,
+document.getElementById("root")
+)
